8dcb5d7f7453d0099c8d3d8f280407de8de8cbf4
Nightrose2016 <lrbarrer@gmail.com>
2020-01-22 13:52:18 -0500

    recipe controller built: create, update, & params

--- Bartenders-flashcards-backend/app/controllers/recipes_controller.rb
+++ Bartenders-flashcards-backend/app/controllers/recipes_controller.rb
@@ -10,4 +10,31 @@ class RecipesController < ApplicationController
         render json: @recipe
     end
     
+    def create
+        @recipe = Recipe.new(params[:recipe])
+        if @recipe.save
+            render json: @recipe
+        else
+            flash[:error] = "Something went wrong"
+            render 'new'
+        end
+
+    end
+    
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update_attributes(recipe_params)
+            render json: @recipe
+        else
+            flash[:error] = "Something went wrong"
+            render 'edit'
+        end
+    end
+    
+    
+    private
+    def recipe_params
+        params.require(:recipe).permit(:body)
+    end
 end
