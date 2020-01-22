7f9b74be19ca495019ef5a3f579f6c18984acd0a
Nightrose2016 <lrbarrer@gmail.com>
2020-01-22 16:55:51 -0500

    commented out bindingeventlistener

--- Bartenders-flashcards-frontend/src/components/recipes.js
+++ Bartenders-flashcards-frontend/src/components/recipes.js
@@ -2,7 +2,7 @@ class Recipes{
     constructor(){
         this.recipes = []
         this.adapter = new RecipesAdapter
-        this.bindEventListeners()
+        // this.bindEventListeners()
         this.fetchAndLoadRecipes()
     }
     fetchAndLoadRecipes(){
