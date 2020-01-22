18d24b6bf3401aea9e48a31b2953b6ed4adc87c4
Nightrose2016 <lrbarrer@gmail.com>
2020-01-22 16:19:25 -0500

    recipes: built fetchandload

--- Bartenders-flashcards-frontend/src/components/recipes.js
+++ Bartenders-flashcards-frontend/src/components/recipes.js
@@ -3,6 +3,11 @@ class Recipes{
         this.recipes = []
         this.adapter = new RecipesAdapter
         this.bindEventListeners()
-        this.fetchAndLoadNotes()
+        this.fetchAndLoadRecipes()
+    }
+    fetchAndLoadRecipes(){
+        this.adapter.getRecipes().then(recipes => {
+            console.log(recipes)
+        })
     }
 }
