9ff66abbf01413bc29aec9dd904c60e750db6043
Nightrose2016 <lrbarrer@gmail.com>
2020-01-22 13:40:00 -0500

    Recipes: index and show methodes built

--- Bartenders-flashcards-backend/app/controllers/recipes_controller.rb
+++ Bartenders-flashcards-backend/app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+    
+    def index
+        @recipes = Recipes.all
+        render json: @recipes
+    end
+    
+    def def show
+        @recipe = Recipe.find(params[:id])
+        render json: @recipe
+    end
+    
 end
