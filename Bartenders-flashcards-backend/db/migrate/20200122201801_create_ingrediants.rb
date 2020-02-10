d98ef9691b3c4bb94843a451fa58ef0078447bac
Nightrose2016 <lrbarrer@gmail.com>
2020-02-10 13:12:03 -0500

    rollback and change ingrediant migration

--- Bartenders-flashcards-backend/db/migrate/20200122201801_create_ingrediants.rb
+++ Bartenders-flashcards-backend/db/migrate/20200122201801_create_ingrediants.rb
@@ -1,6 +1,7 @@
 class CreateIngrediants < ActiveRecord::Migration[6.0]
   def change
     create_table :ingrediants do |t|
+      t.belongs_to :recipe, null: false, foreign_key: true
       t.string :name
       t.timestamps
     end
