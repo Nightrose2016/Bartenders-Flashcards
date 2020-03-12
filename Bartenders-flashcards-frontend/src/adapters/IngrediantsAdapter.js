aaab788c9acec02d6ec5c6d81d59e2242e5c2d26
Nightrose2016 <lrbarrer@gmail.com>
2020-03-12 19:38:42 -0400

    Refactor getIngrediant + built createIngrediant

--- Bartenders-flashcards-frontend/src/adapters/IngrediantsAdapter.js
+++ Bartenders-flashcards-frontend/src/adapters/IngrediantsAdapter.js
@@ -3,8 +3,24 @@ class IngrediantsAdapter {
         this.baseurl = 'http://localhost:3000/ingrediants';
     }
     getIngrediants(){
-        return fetch(this.baseurl)
-        .then(res => res.text())
-        .then(text => console.log(text));
+        return fetch(this.baseurl).then(res => res.json()) ;
+        // .then(text => console.log(text));
     }
+
+    createIngrediant(name_value) {
+        const ingrediant = {
+            name: name_value
+            // parses the info from form then submits it
+        };
+ 
+        return fetch(this.baseurl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ ingrediant }),
+            // shows the submited data from above to the page
+        }).then(res => res.json());
+    }
+
 }
