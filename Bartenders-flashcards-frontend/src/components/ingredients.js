23b721f230100a79b231fb66ca18cd1350c549a6
Nightrose2016 <lrbarrer@gmail.com>
2020-03-21 00:50:55 -0400

    + ID based array.  clrd createIngretient funtion

--- Bartenders-flashcards-frontend/src/components/ingredients.js
+++ Bartenders-flashcards-frontend/src/components/ingredients.js
@@ -13,9 +13,9 @@ class Ingredients{
         this.newIngredientNameGlass = document.getElementById('glass-type') ;
         this.newIngredientNameOne = document.getElementById('first-ingredient');
         this.newIngredientNameTwo = document.getElementById('second-ingredient');
-        this.newIngredientNameThree = document.getElementById('third-ingredient');
+        this.newIngredientNameThree = document.getElementById('third-ingredient');  // refactor for classes to pass into an array
         this.newIngredientNameFour = document.getElementById('fourth-ingredient');
-        this.newIngredientNameGarnish = document.getElementById('fifth-ingredient');
+        this.newIngredientNameFifth = document.getElementById('fifth-ingredient');
         this.recipeID = document.getElementById(recipeID)
         this.ingredientsClass = document.getElementsByClassName('drink-ingredients');
         this.recipeForm.addEventListener('submit', this.createIngredient.bind(this))
@@ -24,16 +24,17 @@ class Ingredients{
     createIngredient(e) {
         e.preventDefault();
 
+        IngredientArray =[]
+        IngredientArray.push(newIngredientNameGlass.value, newIngredientNameOne.value, newIngredientNameTwo.value, newIngredientNameThree.value, newIngredientNameFour.value, newIngredientNameFifth.value) // refactor to moove away from the IDs
+        // needo convert an array to individual ingredients
+        debugger
+        console.log(IngredientArray)
+
         // add the above or switch to an array? array would be better as not having all ingrediands would work
         // const name_value = this.newIngredientName.value ;
 
-        this.adapter.createIngredient(name_value, recipeID)
+        this.adapter.createIngredient()
         .then(Ingredient => {
-            let something = {...ingredient.data.attributes, id: ingredient.data.id}
-            let newIngrediant = new Ingredient (something)
-            this.ingredient.push(new Ingredient(newIngredient));
-            this.newIngredientName.value = '' ;
-            return newIngrediant
         }) ;
     }
     
