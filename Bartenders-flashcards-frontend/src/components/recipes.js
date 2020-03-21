c39a125cf0d7ef282c901f7d44ae78e797085916
Nightrose2016 <lrbarrer@gmail.com>
2020-03-20 21:30:11 -0400

    removed unneded ";"s

--- Bartenders-flashcards-frontend/src/components/recipes.js
+++ Bartenders-flashcards-frontend/src/components/recipes.js
@@ -1,45 +1,45 @@
 class Recipes{
     constructor(){
-        this.recipes = [];
-        this.adapter = new RecipesAdapter() ;
-        this.initBindingsAndEventListeners();
+        this.recipes = []
+        this.adapter = new RecipesAdapter() 
+        this.initBindingsAndEventListeners()
     }
 
     initBindingsAndEventListeners() {
-        this.recipesContainer = document.getElementById('recipes-container');
-        this.body = document.querySelector('body');
-        this.newRecipeName = document.getElementById('new-recipe-name');
-        this.newRecipeBody = document.getElementById('new-recipe-body');
-        this.recipeForm = document.getElementById('new-recipe-form');
-        this.recipeForm.addEventListener('submit', this.createRecipe.bind(this));
-        // this.recipesContainer.addEventListener('dblclick', this.handleRecipeClick.bind(this));
-        // this.body.addEventListener('blur', this.updateRecipe.bind(this), true);
+        this.recipesContainer = document.getElementById('recipes-container')
+        this.body = document.querySelector('body')
+        this.newRecipeName = document.getElementById('new-recipe-name')
+        this.newRecipeBody = document.getElementById('new-recipe-body')
+        this.recipeForm = document.getElementById('new-recipe-form')
+        this.recipeForm.addEventListener('submit', this.createRecipe.bind(this))
+        // this.recipesContainer.addEventListener('dblclick', this.handleRecipeClick.bind(this))
+        // this.body.addEventListener('blur', this.updateRecipe.bind(this), true)
     }
 
     createRecipe(e) {
-        e.preventDefault();
+        e.preventDefault()
         
-        const name_value = this.newRecipeName.value ;
-        const body_value = this.newRecipeBody.value ;
+        const name_value = this.newRecipeName.value
+        const body_value = this.newRecipeBody.value
         
         this.adapter.createRecipe(name_value, body_value).then(recipe => {
-            this.recipes.push(new Recipe(recipe));
-            this.newRecipeName.value = '' ;
-            this.newRecipeBody.value = '' ;
-            this.render() ;
+            this.recipes.push(new Recipe(recipe))
+            this.newRecipeName.value = '' 
+            this.newRecipeBody.value = '' 
+            this.render() 
         }) ;     
-        console.log('your drink is being made');
+        console.log('your drink is being made')
     }
 
     handleRecipeClick(e) {
-        this.toggleRecipe(e) ;
+        this.toggleRecipe(e) 
     }
     
     toggleRecipe(e) {
-        const li = e.target ;
-        li.contentEditable = true ;
-        li.focus() ;
-        li.classList.add('editable') ;
+        const li = e.target
+        li.contentEditable = true 
+        li.focus()
+        li.classList.add('editable') 
     }
 
     // updateRecipe(e) {
@@ -56,12 +56,12 @@ class Recipes{
     fetchAndLoadRecipes = () => {
         this.adapter.getRecipes()
         .then((resp) => {
-            this.recipes = resp.map((obj) => new Recipe(obj)) ;
-            this.render() ;
+            this.recipes = resp.map((obj) => new Recipe(obj))
+            this.render()
         }) ;
     }
 
     render(){
-        this.recipesContainer.innerHTML = this.recipes.map(recipe => recipe.renderDIV()).join('');
+        this.recipesContainer.innerHTML = this.recipes.map(recipe => recipe.renderDIV()).join('')
     }
 }
