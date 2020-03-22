c43cc9f2941b6a69401a735ed3166adb09779771
Nightrose2016 <lrbarrer@gmail.com>
2020-03-22 16:11:29 -0400

    Removed ingredient ids ingredients pulled by class

--- Bartenders-flashcards-frontend/src/components/ingredients.js
+++ Bartenders-flashcards-frontend/src/components/ingredients.js
@@ -9,13 +9,7 @@ class Ingredients{
         this.body = document.querySelector('body');
         this.recipesContainer = document.getElementById('recipes-container');
         this.recipeForm = document.getElementById('new-recipe-form');
-        this.newIngredientNameGlass = document.getElementById('glass-type') ;
-        this.newIngredientNameOne = document.getElementById('first-ingredient');
-        this.newIngredientNameTwo = document.getElementById('second-ingredient');
-        this.newIngredientNameThree = document.getElementById('third-ingredient');  // refactor for classes to pass into an array
-        this.newIngredientNameFour = document.getElementById('fourth-ingredient');
-        this.newIngredientNameFifth = document.getElementById('fifth-ingredient');
-        // this.newIngredientNameClass = document.getElementsByClassName('new-ingredient')
+        this.newIngredientNameClass = document.getElementsByClassName('new-ingredient')
         this.recipeID = document.getElementById(recipeID)
         this.ingredientsClass = document.getElementsByClassName('drink-ingredients');
         this.recipeForm.addEventListener('submit', this.createIngredient.bind(this))
@@ -23,14 +17,10 @@ class Ingredients{
 
     createIngredient(e) {
         e.preventDefault();
-        // const newIngredients = this.newIngredientNameClass
-        // IngredientArray = Array.from(newIngredients.value)
-        // need to convert an array to individual ingredients
-        // add the above or switch to an array? array would be better as not having all ingrediands would work
-        // const name_value = this.newIngredientName.value ;
-        
-        IngredientArray =[]
-        IngredientArray.push(newIngredientNameGlass.value, newIngredientNameOne.value, newIngredientNameTwo.value, newIngredientNameThree.value, newIngredientNameFour.value, newIngredientNameFifth.value) // refactor to moove away from the IDs
+        const newIngredients = this.newIngredientNameClass
+        this.ingredients = Array.from(newIngredients).map(x => x.value)
+        // ingredientsArray.map(value)
+
         debugger
 
 
