a9be3cea9399381e1ef47a84879480ca7d417a91
Nightrose2016 <lrbarrer@gmail.com>
2020-03-22 16:10:23 -0400

    Refactored form to an update

--- Bartenders-flashcards-frontend/src/components/recipe.js
+++ Bartenders-flashcards-frontend/src/components/recipe.js
@@ -19,6 +19,43 @@ class Recipe {
                     <div class="drink-ingredients" data-id=${this.id}> ${this.ingredients().map(ingredient => ingredient.renderLI()).join('')}</div>
 
                 <p class="drink-instruction">${this.body}</p>
+
+                <button type="button" > edit recipe</button>
+
         </div>` ;
-        }
+    }
+
+    renderEDIT() {
+        return `Edit drink
+            Drink Name:<input type="text" name="recipe-name" id="edit-recipe-name">
+
+            <label for="glass">choose a glass:</label>
+            <select id="glass" >
+                <option value="Highball Glass">Highball Glass</option>
+                <option value="Lowball Glass">Lowball Glass</option>
+                <option value="Irish Coffee Glass">Irish Coffee Glass</option>
+                <option value="Cocktail Class">Cocktail Glass</option>
+                <option value="Flute Glass">Flute Glass</option>
+                <option value="Snifter Glass">Sifter Glass</option>
+                <option value=" Hurricane Glass"> Hurricane Glass</option>
+                <option value="Martini Glass">Martini Glass</option>
+                <option value="Margarita Glass">Margarita Glass</option>
+                <option value="The Glencairn Whisky Glass">Glencairn Whisky Glass</option>
+                <option value="Red Wine Glass">Red Wine Glass</option>
+                <option value="White Wine Glass">White Wine Glass</option>
+                <option value="Weizen Glass">Weizen/Wheat Beer Glass</option>
+                <option value="Tulip glass">Tulip glass</option>
+                <option value="Goblet/Chalice Glass">Goblet/Chalice Glass</option>
+                <option value="Pint Glass">Pint Glass</option>
+                <option value="Tumbler Glass">Tumbler Glass</option>
+            </select><br>
+            <input type="text" placeholder="ingredient and measurment" id="first-ingredient" class="new-ingredient">
+            <input type="text" placeholder="ingredient and measurment" id="second-ingredient" class="new-ingredient">
+            <input type="text" placeholder="ingredient and measurment" id="third-ingredient" class="new-ingredient">
+            <input type="text" placeholder="ingredient and measurment" id="fourth-ingredient"class="new-ingredient">
+            <input type="text" placeholder="ingredient and measurment" id="fifth-ingredient" class="new-ingredient"></input>
+            Recipe Body:<input type="text" name="recipe-body" placeholder="Instructions" id="edit-recipe-body"><br>
+            <input type="submit" value="edit recipe" id="submit">
+            `
+    }
 }
