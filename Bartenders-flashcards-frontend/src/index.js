0f5c09b148ee59534ad997508573aab9f278a71a
Nightrose2016 <lrbarrer@gmail.com>
2020-03-24 15:33:38 -0400

    Moved eventlistene for edit click to index

--- Bartenders-flashcards-frontend/src/index.js
+++ Bartenders-flashcards-frontend/src/index.js
@@ -3,4 +3,6 @@ const app = new App();
 app.ingredients.fetchAndLoadIngredients()
     .then(app.recipes.fetchAndLoadRecipes)
 
+document.getElementById('edit-drink').addEventListener('click', function(e){
+    formsContainer.innerHTML = Recipe.renderEDIT()
 console.log('am i alive?');
