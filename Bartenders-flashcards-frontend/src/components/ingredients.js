49194a32d05568f241d7ea3db63da473d9a34f15
Nightrose2016 <lrbarrer@gmail.com>
2020-04-22 17:54:26 -0400

    Removed commented out code

--- Bartenders-flashcards-frontend/src/components/ingredients.js
+++ Bartenders-flashcards-frontend/src/components/ingredients.js
@@ -6,23 +6,14 @@ class Ingredients{
     }
 
     initBindingsAndEventListeners() {
-        // this.body = document.querySelector('body');
-        // this.recipesContainer = document.getElementById('recipes-container');
-        // this.recipeForm = document.getElementById('new-recipe-form');
         this.newIngredientNameClass = document.getElementsByClassName('new-ingredient')
-        // this.recipeID = document.getElementById(recipeID)
         this.ingredientsClass = document.getElementsByClassName('drink-ingredients');
-        // this.recipeForm.addEventListener('submit', this.createIngredient.bind(this))
     }
 
     createIngredient(e) {
         e.preventDefault();
         const newIngredients = this.newIngredientNameClass
         this.ingredients = Array.from(newIngredients).map(x => x.value)
-        // ingredientsArray.map(value)
-
-        debugger
-
 
         this.adapter.createIngredient()
         .then(Ingredient => {
