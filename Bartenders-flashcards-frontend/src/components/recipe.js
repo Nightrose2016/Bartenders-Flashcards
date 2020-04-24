c37ddf774336bf43a31b8be5ce65b02cd625020d
Nightrose2016 <lrbarrer@gmail.com>
2020-04-24 15:34:46 -0400

    Added iteration of drink names to dropdown

--- Bartenders-flashcards-frontend/src/components/recipe.js
+++ Bartenders-flashcards-frontend/src/components/recipe.js
@@ -15,8 +15,11 @@ class Recipe {
                 <h3 data-name=${this.name}>${this.name}</h3>
                     <div class="drink-ingredients" data-id=${this.id}> ${this.ingredients().map(ingredient => ingredient.renderLI()).join('')}</div>
                 <p class="drink-instruction">${this.body}</p>
-                
                 <button class="delete-drink" data-id=${this.id}> delete ${this.name} </button>
                 </div>`
+    }
+    renderrecipeselection() {
+        console.log("recipes are being listed")
+        return `<option value="${this.id}">${this.name}</option>`
     }
 }
